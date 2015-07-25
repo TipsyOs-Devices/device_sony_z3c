32e65d28007bed00d132a27a0e0d6f9c5b58200f
Alx31 <alex31.couderc@gmail.com>
2015-07-25 11:13:02 +0200

    John's pulse opts+ sabermod 4.9 kernel &4.8 on rom

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,17 @@ BOARD_HARDWARE_CLASS += device/sony/z3c/cmhw
 # Kernel properties
 TARGET_KERNEL_CONFIG := cm_shinano_aries_defconfig
 
+# Kernel Toolchain
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9-sm/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+GRAPHITE_OPTS := true
+
+# Rom Toolchain
+TARGET_GCC_VERSION_EXP := 4.8-sm
+
+# John opts
+PULSE := true
+
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 25
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12656242688 # 12656259072 - 16384
