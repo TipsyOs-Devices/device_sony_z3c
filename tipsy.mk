0ad7ca2e962deeb17949bce41f8054e5cdc46d06
Alx31 <alex31.couderc@gmail.com>
2015-08-24 11:16:30 +0200

    tipsy bringup

--- tipsy.mk
+++ tipsy.mk
@@ -1,8 +1,8 @@
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
+# Inherit tipsy common Phone stuff.
+$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/tipsy/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/sony/z3c/full_z3c.mk)
 
@@ -10,5 +10,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D5803
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D5803/D5803:5.1.1/23.4.A.0.546/864948651:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D5803-user 5.1.1 23.4.A.0.546 864948651 release-keys"
 
-PRODUCT_NAME := tesla_z3c
+PRODUCT_NAME := tipsy_z3c
 PRODUCT_DEVICE := z3c
